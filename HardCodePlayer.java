1568c4d87a634d6cac70df561c24803a58ff5d43
Justin Kunimune <jkunimune15@punahou.edu>
2014-10-24 22:41:23 -1000

    A New Victor!
    The good news: I finally got a class that can beat the one-third
    strategy.  The bad news: it’s a hard coded response for every
    possibility from 1 to 20.  So far, no conceivable pattern is visible.

--- HardCodePlayer.java
+++ HardCodePlayer.java
@@ -15,25 +15,54 @@ public class HardCodePlayer extends ChipsPlayer
     switch(total)
     {
       case 1:
-        return 1;
+        return 1; // 1, win
       case 2:
-        return 2;
+        return 2; // 2, win
       case 3:
-        return 1;
+        return 1; // lose
       case 4:
-        return 1;
+        return 1; // 1, win
       case 5:
-        return 1;
+        return 1; // lose
       case 6:
-        return 1;
+        return 1; // 1, win
       case 7:
-        return 2;
+        return 2; // 2, win
       case 8:
-        return 1;
+        return 1; // lose
       case 9:
-        return 1;
+        return 1; // 1, win
       case 10:
-        return 2;
+        return 2; // 2, win
+      case 11:
+        if (max >= 3)
+          return 3; // 3 if possible (if not, lose)
+        else
+          return 1;
+      case 12:
+        return 1; // 1, win
+      case 13:
+        return 1; // lose
+      case 14:
+        return 1; // 1, win
+      case 15:
+        return 2; // 2, win
+      case 16:
+        if (max >= 3)
+          return 3; // 3 if possible (if not, lose)
+        else
+          return 1;
+      case 17:
+        return 1; // 1, win
+      case 18:
+        if (max >= 5)
+          return 5; // 5 if possible (if not, lose)
+        else
+          return 1;
+      case 19:
+        return 1; // 1, win
+      case 20:
+        return 2; // 2, win
       default:
         if (total - total*2/3 - 1 <= max && total - total*2/3 - 1 >= 1)
           return total - total*2/3 - 1;
